a8b9ea8c5af87e80bb67eb3c244b0216fd145393
r.podnajmer <r.podnajmer@gmail.com>
2023-05-11 19:56:43 +0200

    app greetings and maths added to settings py, app maths rewrited to use render and html

--- maths/views.py
+++ maths/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader, Context
 
 
 def math(request):
@@ -10,21 +11,27 @@ def math(request):
 
 def add(request, a, b):
     a, b = int(a), int(b)
-    return HttpResponse(a + b)
+    wynik = a + b
+    c = {"a":a, "b":b, "operacja": "+", "wynik": wynik}
+    return render(request, "maths/main.html", c)
 
 
 def sub(request, a, b):
-    a, b = int(a), int(b)
-    return HttpResponse(a - b)
+    # a, b = int(a), int(b)
+    c = {"a": int(a), "b": int(b), "operacja": "-", "wynik": int(a)-int(b)}
+    return render(request, "maths/main.html", c)
 
 
 def mul(request, a, b):
-    a, b = int(a), int(b)
-    return HttpResponse(a * b)
+    c = {"a": int(a), "b": int(b), "operacja": "*", "wynik": int(a)*int(b)}
+    return render(request, "maths/main.html", c)
 
 
 def div(request, a, b):
-    a, b = int(a), int(b)
-    if b == 0:
-        return HttpResponse("Nie dziel przez 0")
-    return HttpResponse(a / b)
+    c = {"a": int(a), "b": int(b), "operacja": ":", "wynik": "0"}
+    if int(b) == 0:
+        c["wynik"] = "żal i cierpienie, bo nie pamiętałeś, że przez 0 się nie dzieli"
+    else:
+        c["wynik"] = int(a) / int(b)
+
+    return render(request, "maths/main.html", c)
