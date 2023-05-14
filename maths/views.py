e89abfcac1993a353d58eb2c8447ae809b6729f8
r.podnajmer <r.podnajmer@gmail.com>
2023-05-14 19:43:37 +0200

    html for maths app upgrade

--- maths/views.py
+++ maths/views.py
@@ -6,32 +6,32 @@ from django.template import loader, Context
 
 
 def math(request):
-    return HttpResponse("Tu będzie matma")
+    return render(request, "maths/main.html", {})
 
 
 def add(request, a, b):
     a, b = int(a), int(b)
     wynik = a + b
-    c = {"a":a, "b":b, "operacja": "+", "wynik": wynik}
-    return render(request, "maths/main.html", c)
+    c = {"a":a, "b":b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    return render(request, "maths/operations.html", c)
 
 
 def sub(request, a, b):
     # a, b = int(a), int(b)
-    c = {"a": int(a), "b": int(b), "operacja": "-", "wynik": int(a)-int(b)}
-    return render(request, "maths/main.html", c)
+    c = {"a": int(a), "b": int(b), "operacja": "-", "wynik": int(a)-int(b), "title": "dodawanie"}
+    return render(request, "maths/operations.html", c)
 
 
 def mul(request, a, b):
-    c = {"a": int(a), "b": int(b), "operacja": "*", "wynik": int(a)*int(b)}
-    return render(request, "maths/main.html", c)
+    c = {"a": int(a), "b": int(b), "operacja": "*", "wynik": int(a)*int(b), "title": "dodawanie"}
+    return render(request, "maths/operations.html", c)
 
 
 def div(request, a, b):
-    c = {"a": int(a), "b": int(b), "operacja": ":", "wynik": "0"}
+    c = {"a": int(a), "b": int(b), "operacja": ":", "wynik": "0", "title": "dodawanie"}
     if int(b) == 0:
         c["wynik"] = "żal i cierpienie, bo nie pamiętałeś, że przez 0 się nie dzieli"
     else:
         c["wynik"] = int(a) / int(b)
 
-    return render(request, "maths/main.html", c)
+    return render(request, "maths/operations.html", c)
