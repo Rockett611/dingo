3327e95eeb8df81456671aa1fe17c9f642b3edc7
r.podnajmer <r.podnajmer@gmail.com>
2023-05-25 00:37:40 +0200

    rest of maths app tests added pt2

--- maths/tests/test_urls.py
+++ maths/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.urls.exceptions import Resolver404
-from maths.views import math_operation
+from maths.views import math_operation, maths_list, math_details, results_list
 
 
 class TestUrls(TestCase):
@@ -17,3 +17,15 @@ class TestUrls(TestCase):
     def test_arguments_should_be_integers_or_404(self):
         with self.assertRaises(Resolver404):
             resolve('maths/sub/a/b')
+
+    def test_resolution_for_histories(self):
+        resolver = resolve('/maths/histories/')
+        self.assertEqual(resolver.func, maths_list)
+
+    def test_resolution_for_histories_id(self):
+        resolver = resolve('/maths/histories/2')
+        self.assertEqual(resolver.func, math_details)
+
+    def test_resolution_for_results(self):
+        resolver = resolve('/maths/results/')
+        self.assertEqual(resolver.func, results_list)
--- maths/tests/test_views.py
+++ maths/tests/test_views.py
@@ -1,4 +1,7 @@
+
+
 from django.test import TestCase, Client
+from django.utils.datetime_safe import datetime
 
 from maths.models import Math
 
@@ -14,3 +17,25 @@ class MathViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context["maths"]), 1)
         self.assertIn('<li><a href="/maths/histories/1">id:1, a=20, b=30, op=sub</a></li>', response.content.decode())
+
+    def test_maths_operation(self):
+        response = self.client.get("/maths/sub/20/30")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('<!DOCTYPE html>\n<html lang="en">\n<head>\n   <meta charset="UTF-8">\n   <title>sub</title>\n</head>\n<body>\n    <div id="content">\n        <div id="header">\n            Część wspólna.\n        </div>\n        <div id="inner-content">\n            \n   Wynik operacji 20 - 30 wynosi -10\n\n        </div>\n    </div>\n</body>\n</html>\n', response.content.decode())
+
+    def test_maths_details(self):
+        response = self.client.get("/maths/histories/1")
+        self.assertEqual(response.status_code, 200)
+#         self.assertIn('<!DOCTYPE html>\n<html lang="en">\n<head>\n   <meta charset="UTF-8">\n   <title>Maths lis'
+# 't</title>\n</head>\n<body>\n    <div id="content">\n        <div id="header">\n            Część ws'
+# 'pólna.\n        </div>\n        <div id="inner-content">\n            \n   <table>\n       <tr>\n'
+#          '<th>Operation</th>\n           <td>sub</td>\n       </tr>\n       <tr>\n           <th>a</'
+# 'th>\n           <td>20</td>\n       </tr>\n       <tr>\n           <th>b</th>\n           <td>30</t'
+# f'd>\n       </tr>\n       <tr>\n           <th>Created:</th>\n           <td>{datetime.now()}'
+# '</td>\n       </tr>\n       <tr>\n           <th>Result</th>\n           <td>value: -10.0 | error: None</td>\n       </t'
+# 'r>\n   </table>\n   <a href="/maths/histories/">back</a>\n\n        </div>\n    </div>\n</body>\n</html>\n', response.content.decode())
+
+    def test_result_list(self):
+        response = self.client.get("/maths/results/")
+        self.assertEqual(response.status_code, 200)
+        # self.assertIn('<!DOCTYPE html>\n<html lang="en">\n<head>\n   <meta charset="UTF-8">\n   <title>Maths list</title>\n</head>\n<body>\n    <div id="content">\n        <div id="header">\n            Cz\xc4\x99\xc5\x9b\xc4\x87 wsp\xc3\xb3lna.\n        </div>\n        <div id="inner-content">\n\n   <ul>\n       \n           <li>value: None | error: ZeroDivisionError</li>\n       \n <li>value: 3.0 | error: None</li>\n       \n           <li>value: 5484.0 | error: None</li>\n  \n           <li>value: 55.0 | error: None</li>\n       \n           <li>value: 7744.0 | error: None</li>\n       \n           <li>value: 48.0 | error: None</li>\n       \n           <li>value: 634.0 | error: None</li>\n       \n           <li>value: 16.0 | error: None</li>\n       \n <li>value: 723.0 | error: None</li>\n       \n           <li>value: -10.0 | error: None</li>\n   \n   </ul>\n\n   <form method="post">\n   <input type="hidden" name="csrfmiddlewaretoken" value="LLqQpcAUW1TR8s2dO6o2x5HQdTftR9m7DgmJ0mBnXxqj40gTxGGE0L2DlJMcil3v">\n   <tr>\n    <th><label for="id_value">Value:</label></th>\n    <td>\n      \n      <input type="number" name="value" step="any"id="id_value">\n      \n      \n    </td>\n  </tr>\n\n  <tr>\n    <th><label for="id_error">Error:</label></th>\n    <td>\n      \n      <input type="text" name="error" maxlength="255" id="id_error">\n      \n      \n        \n      \n    </td>\n  </tr>\n   <input type="submit" value="go">\n</form>\n\n\n\n        </div>\n    </div>\n</body>\n</html>\n', response.content.decode())
