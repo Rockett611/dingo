1260cc11d265b3680746fedbb009b062248fa3c7
r.podnajmer <r.podnajmer@gmail.com>
2023-05-30 03:13:21 +0200

    template views

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import welcome_view, about_view, contact_view
 
 
 app_name='greetings'
 urlpatterns = [
     path('', welcome_view, name='welcome'),
-    path('about', about_view, name='about'),
-    path('contact', contact_view, name='contact'),
+    # path('about', about_view, name='about'),
+    path('about', TemplateView.as_view(template_name='greetings/about.html'), name='about'),
+    # path('contact', contact_view, name='contact'),
+    path('contact', TemplateView.as_view(template_name='greetings/contact.html'), name='contact'),
 ]
